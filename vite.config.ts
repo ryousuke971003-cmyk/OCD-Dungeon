32eb005cac4b5c8cda271ba1249b83a36cffca1e
ryousuke971003-cmyk <ryousuke971003@gmail>
2026-03-15 05:56:54 +0900

    Refactor: Move index.html and style.css to src folder and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+});
+
